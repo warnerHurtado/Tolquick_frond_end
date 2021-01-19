7ab97235961fcd33761b39d58f023714bfe012f4
warner <warner3898@gmail.com>
2021-01-18 20:22:12 -0600

    Preparando para deploy

---
+++ src/server.js
@@ -0,0 +1,9 @@
+app.use (express.static ('./ dist / tolquick-web-app '));
+
+app.get ('/ *', function (req, res) { 
+  res.sendFile ('index.html', {root: 'dist / tolquick-web-app /'} ); 
+});
+
+app.listen ( proceso .env.PORT || 8080);
+
+
