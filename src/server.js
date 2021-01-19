ff6e556e34562ec21554a7deeabfe6889cc2ac05
warner <warner3898@gmail.com>
2021-01-18 20:31:47 -0600

    reparando server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
-app.use (express.static ('./ dist / tolquick-web-app '));
+app.use (express.static ('./dist/tolquick-web-app'));
 
 app.get ('/ *', function (req, res) { 
-  res.sendFile ('index.html', {root: 'dist / tolquick-web-app /'} ); 
+  res.sendFile ('index.html', {root: 'dist/tolquick-web-app/'} ); 
 });
 
 app.listen ( proceso .env.PORT || 8080);
